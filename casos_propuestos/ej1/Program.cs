4b3465c48039c568a4f4e8d130ed6b9bd2e00464
japp01 <n00407782@upn.pe>
2024-09-12 19:08:42 -0500

    2 casos propuestos subidos

---
+++ casos_propuestos/ej1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ingrese un numero: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int contador = 1;
+int acumulador = 1;
+
+while (contador <= number)
+{
+    acumulador *= contador;
+    contador++;
+}
+Console.WriteLine(acumulador);
---
+++ casos_propuestos/ej2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Ingrese un numero: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+int length = numero.ToString().Length;
+double par = 0, impar = 0, valor;
+byte acumulador = 1;
+int lengthReplica = length;
+
+while (length > 0)
+{
+    valor = numero % 10;
+
+    if (acumulador % 2 != 0)
+    {
+        impar += valor;
+    }
+    else
+    {
+        par += valor;
+    }
+
+    numero /= 10;
+    length--;
+    acumulador++;
+}
+
+Console.WriteLine($"Cantidad de digitos: {lengthReplica}");
+Console.WriteLine($"Suma de digitos impares: {impar}");
+Console.WriteLine($"Suma de digitos pares: {par}");
