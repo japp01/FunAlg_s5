7367f4b64f8fcabc439fb7ec46809bd2633efa2a
l0quend0 <n00407782@upn.pe>
2024-09-18 19:37:55 -0500

    added 3 and 6

---
+++ casos_propuestos/ej3/Program.cs
@@ -0,0 +1,18 @@
+﻿int n;
+double x, denominador = 1, numerador = 0, acumulador = 0;
+
+Console.Write("Ingrese un numero: ");
+x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ingrese la cantidad de terminos de la serie: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    numerador = (Math.Pow(x, i));
+    if (i > 1)
+    {
+        denominador *= i;
+    }
+    acumulador += numerador / denominador;
+    Console.WriteLine(acumulador);
+}
---
+++ casos_propuestos/ej6/Program.cs
@@ -0,0 +1,23 @@
+﻿int n, acumulador = 0;
+
+Console.Write("Ingrese un numero: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Numeros perfectos: ");
+for (int i = 2; i <= n; i++)
+{
+    for (int p = 1; p < i; p++)
+    {
+        if (i % p == 0)
+        {   
+            acumulador += p;
+        }
+
+    }
+
+    if (acumulador == i)
+    {
+        Console.WriteLine(i);
+    }
+    acumulador = 0;
+}
